708de11861fcfc0710e2f7173f5eb986f970b1ee
AbramLiddell <a.liddell@bthstu.catholic.edu.au>
2020-10-30 13:14:50 +1100

    Partially implemented config menu design.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from pygame.locals import *
 from pygame.compat import geterror
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
-assets_dir = os.path.join(main_dir, "assets")
-sounds_dir = os.path.join(assets_dir, "sounds")
-images_dir = os.path.join(assets_dir, "images")
-sprites_dir = os.path.join(images_dir, "sprites")
+assets_dir = os.path.join(main_dir, "Assets")
+sounds_dir = os.path.join(assets_dir, "Sounds")
+images_dir = os.path.join(assets_dir, "Images")
+sprites_dir = os.path.join(images_dir, "Sprites")
 
+print("\nConcentrate: A Game By Abram Liddell")
 print("\nDIRECTORIES:")
 print("     " + main_dir)
 print("     " + assets_dir)
@@ -24,10 +25,12 @@ print("     " + sprites_dir + "\n")
 # Dark Purple - #491d88
 # Light Black/Red - #331a38
 
+# Standardised Fuction For Loading In Images
 def load_image(name, colorkey=None):
     fullname = os.path.join(images_dir, name)
     try:
         image = pg.image.load(fullname)
+        print("STATUS: Image loaded.")
     except pg.error:
         print("ERROR: Cannot load image:", fullname)
         raise SystemExit(str(geterror()))
@@ -36,9 +39,9 @@ def load_image(name, colorkey=None):
         if colorkey == -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey, pg.RLEACCEL)
-    return image, image.get_rect()
+    return image
 
-
+# Standardised Fuction For Loading In Sounds
 def load_sound(name):
     class NoneSound:
         def play(self):
@@ -53,7 +56,8 @@ def load_sound(name):
         print("Cannot load sound: %s" % fullname)
         raise SystemExit(str(geterror()))
     return sound
-    
+
+# Class Controlling The Player
 class Player(pg.sprite.Sprite):
 
     def __init__(self):
@@ -74,9 +78,9 @@ class Player(pg.sprite.Sprite):
                 self.image = pg.transform.flip(self.image, 1, 0)
             self.rect = newpos
 
-
+# Main Sub Which Controls All Other Game Functions
 def main():
-    # Initialize Everything
+    # Initialize Everything Required For Game Operation
     screen = pg.display.set_mode((1280, 720))
     pg.display.set_caption("Concentrate")
     pg.mouse.set_visible(1)
@@ -141,25 +145,32 @@ def mainMenu(mainMenuSurface, screen, clock):
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if mousePos[0] >= startpos.topleft[0] and mousePos[0] <= startpos.bottomright[0] and mousePos[1] <= startpos.bottomright[1] and mousePos[1] >= startpos.topleft[1]:
                     menuGoing = False
-                    print("Start Game")
+                    print("CONTROL: Start Game")
                 elif mousePos[0] >= instructionpos.topleft[0] and mousePos[0] <= instructionpos.bottomright[0] and mousePos[1] <= instructionpos.bottomright[1] and mousePos[1] >= instructionpos.topleft[1]:
-                    print("Instructions")
+                    print("CONTROL: Instructions")
                 
 
 def configMenu(configMenuSurface, screen, clock):
+    
+    # Background
+    configBackgroundImage = load_image("brick-wall-1280x720.jpg")
+    configRect = configBackgroundImage.get_rect()
+    configMenuSurface.blit(configBackgroundImage, configRect)
+
     # Title Text
     font = pg.font.Font(None, 55)
     text = font.render("Game Configuration", 1, (10, 10, 10))
-    textpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round(configMenuSurface.get_height()/3))
+    textpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round((configMenuSurface.get_height()/3) - 100))
     configMenuSurface.blit(text, textpos)
 
     # Start Game Text
-    font = pg.font.Font(None, 30)
-    text = font.render("Start Game", 1, (10, 10, 10))
-    startpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round(configMenuSurface.get_height()/2))
+    font = pg.font.Font(None, 40)
+    text = font.render("Start", 1, (10, 10, 10))
+    startpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=670)
     configMenuSurface.blit(text, startpos)
 
     # Instructions Text
+    font = pg.font.Font(None, 30)
     text = font.render("Instructions", 1, (10, 10, 10))
     instructionpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round(configMenuSurface.get_height()/2) + round(1.5*(startpos.bottomright[1]-startpos.topleft[1])))
     configMenuSurface.blit(text, instructionpos)
@@ -181,6 +192,7 @@ def configMenu(configMenuSurface, screen, clock):
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if mousePos[0] >= startpos.topleft[0] and mousePos[0] <= startpos.bottomright[0] and mousePos[1] <= startpos.bottomright[1] and mousePos[1] >= startpos.topleft[1]:
                     menuGoing = False
+                    print("CONTROL: Config Start")
     
 def startupChecks():
     if not pg.font:
