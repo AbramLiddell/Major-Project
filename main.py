61a9912984f46a1191af029551c66a715778ecb2
AbramLiddell <a.liddell@bthstu.catholic.edu.au>
2021-01-18 13:49:14 +1100

    Added the base for movement, and enabled visible player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pygame as pg
 import sys
+import time
 from pygame.locals import *
 from pygame.compat import geterror
 
@@ -18,6 +19,12 @@ print("     " + sounds_dir)
 print("     " + images_dir)
 print("     " + sprites_dir + "\n")
 
+global gameStarted
+global bordernum
+bordernum = 0
+gameStarted = False
+border = []
+
 # Colour Pallette:
 # Pink - #fa448c
 # Yellow - #fec859
@@ -28,6 +35,23 @@ print("     " + sprites_dir + "\n")
 # Standardised Fuction For Loading In Images
 def load_image(name, colorkey=None):
     fullname = os.path.join(images_dir, name)
+    try:
+        image = pg.image.load(fullname)
+        print("STATUS: Image loaded.")
+    except pg.error:
+        print("ERROR: Cannot load image:", fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, pg.RLEACCEL)
+    return image
+
+    # Standardised Fuction For Loading In Sprites
+def load_sprite(name, colorkey=None):
+    fullname = os.path.join(sprites_dir, name)
+    print("Sprite Name: " + fullname)
     try:
         image = pg.image.load(fullname)
         print("STATUS: Image loaded.")
@@ -62,12 +86,15 @@ class Player(pg.sprite.Sprite):
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)  # call Sprite intializer
-        self.image, self.rect = load_image("player.png", -1)
+        print("Loading Sprite")
+        self.image = load_sprite("square.png", -1)
+        self.rect = self.image.get_rect()
         screen = pg.display.get_surface()
         self.area = screen.get_rect()
         self.rect.topleft = 10, 10
         self.move = 9
-
+    def update(self):
+        print("Updated.")
     def move(self):
         # Move according to mouse position
         newpos = self.rect.move((self.move, 0))
@@ -77,8 +104,14 @@ class Player(pg.sprite.Sprite):
                 newpos = self.rect.move((self.move, 0))
                 self.image = pg.transform.flip(self.image, 1, 0)
             self.rect = newpos
+class Border():
+    global bordernum
+    def __init__(self):
+        self.x = generateX()
+        self.y = generateY()
+        self.width = generateWidth()
 
-# Loads images with transparency
+# Loads Images With Transparency
 def blit_alpha(target, source, location, opacity):
         x = location[0]
         y = location[1]
@@ -90,6 +123,10 @@ def blit_alpha(target, source, location, opacity):
 
 # Main Sub Which Controls All Other Game Functions
 def main():
+
+    global gameStarted
+    global bordernum
+
     # Initialize Everything Required For Game Operation
     screen = pg.display.set_mode((1280, 720))
     pg.display.set_caption("Concentrate")
@@ -111,24 +148,102 @@ def main():
     instructionMenuSurface = instructionMenuSurface.convert()
     instructionMenuSurface.fill((250, 250, 250))
 
+    # Create The Game Surface
+    gameSurface = pg.Surface(screen.get_size())
+    gameSurface = gameSurface.convert()
+    gameSurface.fill((250, 250, 250))
+
     # Call The Various Menus
-    instructionsTripped = False
-    while True:
-        mainMenu(mainMenuSurface, screen, clock, instructionMenuSurface, instructionsTripped)
-        if instructionsTripped == True:
-            break
+    while not gameStarted:
+        mainMenu(mainMenuSurface, screen, clock, instructionMenuSurface)
 
     configMenu(configMenuSurface, screen, clock)
+    startTime = getPerfTime()
 
-    print("GAME STATUS: Running")
-    # Prepare Game Objects
+    print("\nGAME STATUS: Running\n")
+    print("Time Started At: " + str(startTime))
+    drawGame(gameSurface, screen, clock)
     
+    # Prepare Game Objects
     #button_sound = load_sound("button_sound.wav")
     #barrier_sound = load_sound("barrier_sound.wav")
     #death_sound = load_sound("death_sound.wav")
-    #player = Player()
+    
+    player = Player()
 
-def mainMenu(mainMenuSurface, screen, clock, instructionMenuSurface, instructionsTripped):
+    currentTimer = getPerfTime()
+    gameTime = currentTimer - startTime
+
+    print("Game Time: " + str(gameTime))
+    #have game countdown here
+    #while (gameTime <= 3):
+    playerPosition = 600
+    border.append(Border())
+    allSprites = pg.sprite.RenderPlain((player))
+    screen.blit(gameSurface, (0, 0))
+    allSprites.draw(screen)
+    pg.display.flip()
+
+    while True:
+        for event in pg.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == KEYDOWN:
+                if event.key == K_w:
+                    player.moveup()
+                if event.key == K_s:
+                    player.movedown()
+                if event.key == K_a:
+                    player.moveleft()
+                if event.key == K_d:
+                    player.moveright()
+            elif event.type == KEYUP:
+                if event.key == K_a or event.key == K_z:
+                    player.movepos = [0,0]
+                    player.state = "still"
+
+        bordernum += 1
+        border.append(Border())
+        allSprites.update()
+        screen.blit(gameSurface, (0, 0))
+        allSprites.draw(screen)
+        pg.display.flip()
+        pg.time.Clock().tick(1)
+    
+    
+    
+    #print(border[0].x)
+    #print(border[0].y)
+    #print(border[0].width)
+    #print(border[bordernum].x)
+    #print(border[bordernum].y)
+    #print(border[bordernum].width)
+
+def getPerfTime():
+    return time.perf_counter()
+
+def generateX():
+    global bordernum
+    if bordernum == 0:
+        #return borderwidth
+        return 1000
+    else:
+        return border[bordernum-1].x - 100
+
+def generateY():
+    # randomboi
+    return 100
+
+def generateWidth():
+    # randomboi 
+    return 100
+
+# Main Menu Function
+def mainMenu(mainMenuSurface, screen, clock, instructionMenuSurface):
+    global gameStarted
+
+    # Fill white to stop bolding - clear screen
+    mainMenuSurface.fill((250, 250, 250))
 
     # Title Text
     font = pg.font.Font(None, 55)
@@ -164,45 +279,29 @@ def mainMenu(mainMenuSurface, screen, clock, instructionMenuSurface, instruction
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if mousePos[0] >= startpos.topleft[0] and mousePos[0] <= startpos.bottomright[0] and mousePos[1] <= startpos.bottomright[1] and mousePos[1] >= startpos.topleft[1]:
                     menuGoing = False
+                    gameStarted = True
                     print("CONTROL: Start Game")
                 elif mousePos[0] >= instructionpos.topleft[0] and mousePos[0] <= instructionpos.bottomright[0] and mousePos[1] <= instructionpos.bottomright[1] and mousePos[1] >= instructionpos.topleft[1]:
                     print("CONTROL: Instructions")
                     instructionMenu(instructionMenuSurface, screen, clock)
-                    instructionsTripped = True
                     menuGoing = False
 
-                    #have sub go back to the main loop and check if instructions has been hit. If yes , than main menu again.
-    
+# Configuration Menu Function
 def configMenu(configMenuSurface, screen, clock):
     
+    # Fill white to stop bolding - clear screen
+    configMenuSurface.fill((250, 250, 250))
+
     # Background
-    configBackgroundImage = load_image("brick-wall-1280x720.jpg")
+    configBackgroundImage = load_image("Config-Menu.png")
     configRect = configBackgroundImage.get_rect()
-    configMenuSurface.blit(configBackgroundImage, configRect)
+    blit_alpha(configMenuSurface, configBackgroundImage, configRect, 255)
 
-    # Title Text
-    configTitleImage = load_image("config-menu-text.png")
-    configRect = configBackgroundImage.get_rect()
-    blit_alpha(configMenuSurface, configTitleImage, configRect, 10)
-
-    # Title Text
-    #font = pg.font.Font(None, 40)
-    #text = font.render("Configuration", 1, (255, 255, 255))
-    #startpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round(configMenuSurface.get_height()/3 - 100))
-    
-
-    #configMenuSurface.blit(text, startpos)
     # Start Game Text
     font = pg.font.Font(None, 40)
-    text = font.render("Start", 1, (10, 10, 10))
+    text = font.render("Start", 1, (250, 250, 250))
     startpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=670)
     configMenuSurface.blit(text, startpos)
-
-    # Instructions Text
-    font = pg.font.Font(None, 30)
-    text = font.render("Instructions", 1, (10, 10, 10))
-    instructionpos = text.get_rect(centerx=round(configMenuSurface.get_width() / 2), centery=round(configMenuSurface.get_height()/2) + round(1.5*(startpos.bottomright[1]-startpos.topleft[1])))
-    configMenuSurface.blit(text, instructionpos)
 
     # Display The Surface
     screen.blit(configMenuSurface, (0, 0))
@@ -223,8 +322,12 @@ def configMenu(configMenuSurface, screen, clock):
                     menuGoing = False
                     print("CONTROL: Config Start")
     
+# Instruction Menu Function
 def instructionMenu(instructionMenuSurface, screen, clock):
-    
+
+    # Fill white to stop bolding - clear screen
+    instructionMenuSurface.fill((250, 250, 250))
+
     # Title Text
     font = pg.font.Font(None, 40)
     TitleText = font.render("Instructions", 1, (10, 10, 10))
@@ -257,13 +360,25 @@ def instructionMenu(instructionMenuSurface, screen, clock):
                 if mousePos[0] >= backpos.topleft[0] and mousePos[0] <= backpos.bottomright[0] and mousePos[1] <= backpos.bottomright[1] and mousePos[1] >= backpos.topleft[1]:
                     menuGoing = False
                     print("CONTROL: Back")
+
+def drawGame(gameSurface, screen, clock):
+    # Fill white to stop bolding - clear screen
+    gameSurface.fill((250, 250, 250))
+
+    # Background
+    #gameBackgroundImage = load_image("Config-Menu.png")
+    #gameRect = gameBackgroundImage.get_rect()
+    #blit_alpha(gameSurface, gameBackgroundImage, gameRect, 255)
+
+    
+    screen.blit(gameSurface, (0, 0))
+    pg.display.flip()
+# Startup Function to Ensure Modules Are Loaded
 def startupChecks():
     if not pg.font:
         print("WARNING: Fonts disabled.")
     if not pg.mixer:
         print("WARNING: Sounds disabled.")
-
-
 if __name__ == "__main__":
     pg.init()
     startupChecks()
